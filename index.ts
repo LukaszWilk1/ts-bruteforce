e95aa01f520941ee2e6e9b54258b052d7cfa8d5f
Lukasz <lukasz.wilk231@gmail.com>
2024-04-04 20:46:43 +0200

    changed getting data functionality into function

--- index.ts
+++ index.ts
@@ -1,17 +1,24 @@
 import * as fs from 'fs';
 import { citiesNodes } from './shared/citiesNodes';
 
-const nodes: string[] = fs.readFileSync('data.txt', 'utf-8').split('\r\n');
-let citiesNodesArray: citiesNodes[] = []; 
+let roads: citiesNodes[] = [];
 
-for(let i: number = 0; i < nodes.length; i++){
-    const [from, to, time] = nodes[i].split(" ");
-    const cityNode: citiesNodes = {from: '', to: '', time: ''};
-    cityNode.from = from;
-    cityNode.to = to;
-    cityNode.time = time;
-    citiesNodesArray.push(cityNode);
+const getData = () : citiesNodes[] => {
+    const nodes: string[] = fs.readFileSync('data.txt', 'utf-8').split('\r\n');
+    let citiesNodesArray: citiesNodes[] = []; 
+
+    for(let i: number = 0; i < nodes.length; i++){
+        const [from, to, time] = nodes[i].split(" ");
+        const cityNode: citiesNodes = {from: '', to: '', time: ''};
+        cityNode.from = from;
+        cityNode.to = to;
+        cityNode.time = time;
+        citiesNodesArray.push(cityNode);
+    }
+
+    return citiesNodesArray;
 }
 
 
-console.log(citiesNodesArray);
+
+roads = getData();
