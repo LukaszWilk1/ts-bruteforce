90391156e3616811f399fda30f4aa7a761142159
Lukasz <lukasz.wilk231@gmail.com>
2024-04-05 15:12:50 +0200

    created array of cities

--- index.ts
+++ index.ts
@@ -6,6 +6,9 @@ let roads: citiesNodes[] = [];
 const getData = () : citiesNodes[] => {
     const nodes: string[] = fs.readFileSync('data.txt', 'utf-8').split('\r\n');
     let citiesNodesArray: citiesNodes[] = []; 
+    let cities: string[] = [];
+
+    const [nodesAmmount, startNode] = nodes.shift().split(" ");
 
     for(let i: number = 0; i < nodes.length; i++){
         const [from, to, time] = nodes[i].split(" ");
@@ -16,9 +19,26 @@ const getData = () : citiesNodes[] => {
         citiesNodesArray.push(cityNode);
     }
 
+    console.log(startNode, nodesAmmount);
+
+    cities.push(citiesNodesArray[0].from);
+    cities.push(citiesNodesArray[0].to);
+
+    for(let i: number = 1; i < citiesNodesArray.length; i++){
+        const previous = citiesNodesArray[i-1].to;
+        const current =  citiesNodesArray[i].to;
+        if(!cities.includes(citiesNodesArray[i].to) && !cities.includes(citiesNodesArray[i].from)){
+            cities.push(citiesNodesArray[i].to);
+            cities.push(citiesNodesArray[i].from);
+        } 
+    }
+
+    console.log(cities);
+
     return citiesNodesArray;
 }
 
 
 
 roads = getData();
+console.log(roads);
